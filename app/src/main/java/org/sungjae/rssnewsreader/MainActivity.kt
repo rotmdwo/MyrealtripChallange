d3260ed53cb243bb98decb8f5584e8cd629e1c1c
rotmdwo <49435944+rotmdwo@users.noreply.github.com>
2020-03-29 13:47:27 +0900

    1.3초 후 스플래시 화면에서 뉴스리스트 화면으로 이동 추가

--- app/src/main/java/org/sungjae/rssnewsreader/MainActivity.kt
+++ app/src/main/java/org/sungjae/rssnewsreader/MainActivity.kt
@@ -1,12 +1,24 @@
 package org.sungjae.rssnewsreader
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handler = Handler()
+        val delayedTime: Long = 1300
+        val task = object : Runnable{
+            override fun run(){
+                val intent = Intent(applicationContext,NewsListActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        handler.postDelayed(task,delayedTime)
     }
 }
---
+++ app/src/main/java/org/sungjae/rssnewsreader/NewsListActivity.kt
@@ -0,0 +1,12 @@
+package org.sungjae.rssnewsreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_list)
+    }
+}
