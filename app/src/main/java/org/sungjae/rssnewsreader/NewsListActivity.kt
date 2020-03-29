0c5de32c07a7903329198336638c19fa577081ee
rotmdwo <49435944+rotmdwo@users.noreply.github.com>
2020-03-30 02:40:50 +0900

    멀티쓰레드로 뉴스리스트 불러오기 속도 향상

--- app/src/main/java/org/sungjae/rssnewsreader/NewsListActivity.kt
+++ app/src/main/java/org/sungjae/rssnewsreader/NewsListActivity.kt
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import kotlinx.android.synthetic.main.activity_news_list.*
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 import java.io.IOException
 
 class NewsListActivity : AppCompatActivity() {
@@ -20,8 +22,6 @@ class NewsListActivity : AppCompatActivity() {
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         recyclerView.layoutManager = layoutManager
 
-
-
         val mHandler = Handler()
         getNewsInThread(mHandler,adapter)
 
@@ -33,87 +33,95 @@ class NewsListActivity : AppCompatActivity() {
         }
     }
 
-    fun getNewsInThread(mHandler: Handler, adapter: NewsAdapter){
+    private fun getNewsInThread(mHandler: Handler, adapter: NewsAdapter){
+        var items: Elements = Elements()
         Thread(Runnable {
 
             try {
-                val doc = Jsoup.connect("https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko").timeout(5000).get()
-                val items = doc.select("rss channel item")
-                for(item in items){
-                    val keyWords = Array<String>(3, init = { index -> "" })
-
-                    val title = item.select("title").text()
-                    val link = item.select("link").text()
-                    val source = item.select("source").text()
-                    if(source.equals("한국경제")){
-                        Log.d("","SSLprotocolexception를 일으키는 한국경제 뉴스는 가져오지 않음")
-                        continue
-                    }
-                    val docOfLink = Jsoup.connect(link)
-                        .timeout(5000)
-                        .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
-                        .ignoreHttpErrors(true).get()
-                    val description = docOfLink.select("head meta[property=og:description]").attr("content")
-                    val imageURL = docOfLink.select("head meta[property=og:image]").attr("content")
-
-                    if(description.equals("")){ // meta[property=og:description] 태그 없으면
-                    } else{
-                        val words = description.split(" ")
-                        val wordsIterator = words.iterator()
-                        val wordsLongerThan2Letters = ArrayList<String>()
-                        var i = 0
-                        while(wordsIterator.hasNext()){
-                            val wordLongerThan2Letter = wordsIterator.next()
-                            if(wordLongerThan2Letter.length>=2){
-                                wordsLongerThan2Letters.add(wordLongerThan2Letter)
-                            }
-                        }
-                        val set = wordsLongerThan2Letters.toSet()
-                        //val set = words.toSet()
-                        //val nonRepeatedWords = Array<String>(set.size,init = { index -> "" })
-                        //val numOfRepeats = Array<Int>(set.size,init = {index -> 0})
-
-                        var nonRepeatedWords = Array<Words>(set.size,init={index -> Words("",0)})
-
-                        val iterator = set.iterator()
-                        var index = 0
-                        while(iterator.hasNext()){
-                            //nonRepeatedWords[index++] = iterator.next()
-                            nonRepeatedWords[index++].nonRepeatedWord = iterator.next()
-                        }
-                        for(i in 0 until nonRepeatedWords.size){
-                            for(j in 0 until words.size){
-                                if(nonRepeatedWords[i].nonRepeatedWord.equals(words[j])){
-                                    //numOfRepeats[i]++
-                                    nonRepeatedWords[i].numOfRepeats++
-                                }
-                            }
-                        }
-                        nonRepeatedWords.sortWith(kotlin.Comparator<Words>{ a: Words, b:Words ->
-                            when{
-                                a.numOfRepeats > b.numOfRepeats -> -1
-                                a.numOfRepeats < b.numOfRepeats -> 1
-                                else -> a.nonRepeatedWord.compareTo(b.nonRepeatedWord)
-                            }
-                        })
-
-                        if(!nonRepeatedWords[0].nonRepeatedWord.equals("")) keyWords[0] = nonRepeatedWords[0].nonRepeatedWord
-                        if(!nonRepeatedWords[1].nonRepeatedWord.equals("")) keyWords[1] = nonRepeatedWords[1].nonRepeatedWord
-                        if(!nonRepeatedWords[2].nonRepeatedWord.equals("")) keyWords[2] = nonRepeatedWords[2].nonRepeatedWord
-
-                        if(imageURL.equals("")){ // meta[property=og:image] 태그 없으면
-                        } else{
-                            adapter.addItem(News(title = title,description = description,keywords = keyWords,link = link,image = imageURL))
-                        }
-                    }
-                }
+                val doc = Jsoup.connect("https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko").timeout(50000).get()
+                val tempItems = doc.select("rss channel item")
+                items = tempItems
             }catch (e : IOException){
-                Log.d("asdf","1")
                 e.printStackTrace()
             }
             mHandler.post{
-                recyclerView.adapter = adapter
+                for(item in items){
+                    Thread(Runnable{
+                        try{
+                            val keyWords = Array<String>(3, init = { index -> "" })
+
+                            val title = item.select("title").text()
+                            val link = item.select("link").text()
+                            val source = item.select("source").text()
+                            if(source.equals("한국경제")){
+                                Log.d("","SSLprotocolexception를 일으키는 한국경제 뉴스는 가져오지 않음")
+
+                            }
+                            val docOfLink = Jsoup.connect(link)
+                                .timeout(50000)
+                                .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                                .ignoreHttpErrors(true).get()
+                            val description = docOfLink.select("head meta[property=og:description]").attr("content")
+                            val imageURL = docOfLink.select("head meta[property=og:image]").attr("content")
+
+                            if(description.equals("")){ // meta[property=og:description] 태그 없으면
+                            } else{
+                                val words = description.split(" ")
+                                val wordsIterator = words.iterator()
+                                val wordsLongerThan2Letters = ArrayList<String>()
+                                var i = 0
+                                while(wordsIterator.hasNext()){
+                                    val wordLongerThan2Letter = wordsIterator.next()
+                                    if(wordLongerThan2Letter.length>=2){
+                                        wordsLongerThan2Letters.add(wordLongerThan2Letter)
+                                    }
+                                }
+                                val set = wordsLongerThan2Letters.toSet()
+
+                                var nonRepeatedWords = Array<Words>(set.size,init={index -> Words("",0)})
+
+                                val iterator = set.iterator()
+                                var index = 0
+                                while(iterator.hasNext()){
+                                    //nonRepeatedWords[index++] = iterator.next()
+                                    nonRepeatedWords[index++].nonRepeatedWord = iterator.next()
+                                }
+                                for(i in 0 until nonRepeatedWords.size){
+                                    for(j in 0 until words.size){
+                                        if(nonRepeatedWords[i].nonRepeatedWord.equals(words[j])){
+                                            //numOfRepeats[i]++
+                                            nonRepeatedWords[i].numOfRepeats++
+                                        }
+                                    }
+                                }
+                                nonRepeatedWords.sortWith(kotlin.Comparator<Words>{ a: Words, b:Words ->
+                                    when{
+                                        a.numOfRepeats > b.numOfRepeats -> -1
+                                        a.numOfRepeats < b.numOfRepeats -> 1
+                                        else -> a.nonRepeatedWord.compareTo(b.nonRepeatedWord)
+                                    }
+                                })
+
+                                if(!nonRepeatedWords[0].nonRepeatedWord.equals("")) keyWords[0] = nonRepeatedWords[0].nonRepeatedWord
+                                if(!nonRepeatedWords[1].nonRepeatedWord.equals("")) keyWords[1] = nonRepeatedWords[1].nonRepeatedWord
+                                if(!nonRepeatedWords[2].nonRepeatedWord.equals("")) keyWords[2] = nonRepeatedWords[2].nonRepeatedWord
+
+                                if(imageURL.equals("")){ // meta[property=og:image] 태그 없으면
+                                } else{
+                                    adapter.addItem(News(title = title,description = description,keywords = keyWords,link = link,image = imageURL))
+                                }
+                            }
+                        } catch (e: IOException){
+                            e.printStackTrace()
+                        }
+                        mHandler.post{
+                            recyclerView.adapter = adapter
+                        }
+                    }).start()
+                }
             }
         }).start()
+
+
     }
 }
