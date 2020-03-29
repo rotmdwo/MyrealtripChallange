e7bf41bc16dcc2846d09c686636c5fb7cbb6d5e9
rotmdwo <49435944+rotmdwo@users.noreply.github.com>
2020-03-29 16:21:04 +0900

    기본적인 jsoup 적용

--- app/src/main/java/org/sungjae/rssnewsreader/NewsListActivity.kt
+++ app/src/main/java/org/sungjae/rssnewsreader/NewsListActivity.kt
@@ -2,11 +2,42 @@ package org.sungjae.rssnewsreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import org.jsoup.Jsoup
+import java.io.IOException
 
 class NewsListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_list)
+        val mHandler = Handler()
+        Thread(Runnable {
+            var data = ""
+            try {
+                val doc = Jsoup.connect("https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko").timeout(5000).get()
+                val items = doc.select("rss channel item")
+                for(item in items){
+                    val title = item.select("title").text()
+                    val link = item.select("link").text()
+                    Log.d("asdf",link)
+                    val docOfLink = Jsoup.connect(link).timeout(5000).get()
+                    val description = docOfLink.select("head meta[property=og:description]").attr("content")
+                    if(description.equals("")){
+                        Log.d("asdf","2")
+                    } else{
+                        Log.d("asdf",description)
+                    }
+
+                }
+            }catch (e : IOException){
+                Log.d("asdf","1")
+                e.printStackTrace()
+            }
+            mHandler.post{
+
+            }
+        }).start()
     }
 }
--- app/src/test/java/org/sungjae/rssnewsreader/ExampleUnitTest.kt
+++ app/src/test/java/org/sungjae/rssnewsreader/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package org.sungjae.rssnewsreader
 
+import android.os.Handler
+import android.util.Log
+import org.jsoup.Jsoup
+import org.junit.Assert
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.IOException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +18,18 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun crawllingTest(){
+        val mHandler = Handler()
+        val doc = Jsoup.connect("https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko").get()
+        val items = doc.select("rss channel item")
+        var data = ""
+        for(item in items){
+            val link = item.select("link").text()
+            data = link
+        }
+        assertEquals("https://news.google.com/",data.substring(0,24))
     }
 }
